65e9dd2e4e63d5998772be794bc0c24fd7d2f9cf
bvinha <104568528+bvinha@users.noreply.github.com>
2025-03-12 16:44:10 +0100

    Update 04_Extract-Video-Frames-Annotations.py
    - Adapted code to extract frames where specific labels (e.g., species) were annotated on video, based on the BIIGLE video annotation report (in .csv). 
    - Added extra comments and explanations to code for improved reproducibility

--- 04_Extract-Video-Frames-Annotations.py
+++ 04_Extract-Video-Frames-Annotations.py
@@ -1,44 +1,50 @@
+"""
+Run code to extract video frames based on the "label_name" column from BIIGLE annotations report
+"""
+#import libraries
 import cv2
 import pandas as pd
 import os
 
-def extract_frames(video_path, csv_path, output_folder):
-    # Leer el archivo CSV
-    df = pd.read_csv(csv_path, delimiter=';', decimal=',')
+#build 'extract_frames' function
+def extract_frames(video_path, csv_path, output_folder, target_label):
+    #read csv file
+    df = pd.read_csv(csv_path, delimiter=';', decimal=',') #change according to your computer settings, e.g. delimiter= ',', decimal='.'
 
-    # Asegurarse de que la carpeta de salida existe
+    #filter by "label_name" in BIIGLE annotation report
+    filtered_df = df[df['label_name'] == target_label]
+
+    #make sure output_folder exists
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    # Abrir el video
+    #open video
     cap = cv2.VideoCapture(video_path)
 
-    # Iterar sobre cada fila del DataFrame
-    for index, row in df.iterrows():
-        # Obtener el segundo del video
-        second = row['Frame']
-        print(f"second: {second}")
-    
+    #iterate each row in the filtered dataframe
+    for index, row in filtered_df.iterrows():
+        #get frame number from video
+        frame_number = int(row['frames']) #change column name, if different
+        print(f"Frame: {frame_number}")
 
+        #calculate corresponding frame
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
 
-        # Calcular el frame correspondiente
-        cap.set(cv2.CAP_PROP_POS_MSEC, second*1000)
-
-        # Leer el frame
+        #Read frame
         ret, frame = cap.read()
 
-        # Si el frame se leyó correctamente, guardarlo como imagen PNG
+        #Save frame as PNG
         if ret:
-            cv2.imwrite(os.path.join(output_folder, f'{second}.png'), frame)
-            print(f"Done: {second}")
+            cv2.imwrite(os.path.join(output_folder, f'{row["video_filename"]}_frame{frame_number}.png'), frame) #change column name, if different
+            print(f"Done: {frame_number}")
 
-    # Liberar el video
+    #Release video
     cap.release()
 
-# Uso de la función
-#extract_frames('video.mp4', 'data.csv', 'frames')
-extract_frames('/Volumes/LaCie/OASIS_ROV_VIDEOS/Dive15a.avi', 'oasis15_selected.csv', '/Users/arianmartinez/Desktop/oasis15')
-
-
+#Function Use
+#Extract all frames annotated with "Eunicella singularis", from a video file called "video.mp4", 
+#based on an annotation report with file name "annotation_report.csv". 
+#Frames will be saved to folder named "frames"
+extract_frames('video.mp4', 'annotation_report.csv', 'frames', 'Eunicella singularis') #change according to your directory/filenames
 
 
